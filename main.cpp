4d0b4b86a5423b793972d09947083e66a8c23c23
oliwilliams1 <imhungryoliver@gmail.com>
2024-11-25 16:11:55 +1300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// PlanetRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "PlanetRenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
