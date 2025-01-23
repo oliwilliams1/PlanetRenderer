74416c5747c14914bcf6a6e56b552627344cdf8a
oliwilliams1 <imhungryoliver@gmail.com>
2025-01-23 20:45:25 +1300

    Add icon to executable

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,8 @@ int main() {
 	app.Mainloop();
 	return 0;
 }
+
+#ifdef _WIN32
+#include <Windows.h>
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) { return main(); }
+#endif
